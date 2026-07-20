4600403696a72ed30ac78ed13ce627d7227750e3
Kadir Khan <kk8045433@gmail.com>
2026-07-21 01:30:23 +0530

    Fix Render deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
+    console.log("MongoDB Connection Error:");
     console.log(error);
     process.exit(1);
   }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
 const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const connectDB = require('./config/db.js');
-require('dotenv').config({ path: "./config.env" });
-const messageRoutes = require("../src/routes/message");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const UserRoutes = require("./routes/user");
+const PostRoutes = require("./routes/post");
+const NotificationRoutes = require("./routes/notification");
+const CommentRoutes = require("./routes/comment");
+const messageRoutes = require("./routes/message");
+
 const app = express();
-const PORT = process.env.PORT || 400
-// Middlewares
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cookieParser());
-// connected to databse 
+
+const PORT = process.env.PORT || 4000;
+
+// Connect Database
 connectDB();
 
-const UserRoutes = require('./routes/user');
-const PostRoutes = require('./routes/post');
-const NotificationRoutes = require('./routes/notification');
-const CommentRoutes = require('./routes/comment');
+// Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", UserRoutes);
 app.use("/api/post", PostRoutes);
-app.use("/api/notification",NotificationRoutes);
-app.use("/api/comment",CommentRoutes);
+app.use("/api/notification", NotificationRoutes);
+app.use("/api/comment", CommentRoutes);
 app.use("/api/message", messageRoutes);
+
 console.log("Message route registered");
 
-// Test route
+// Test Route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Server start
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
